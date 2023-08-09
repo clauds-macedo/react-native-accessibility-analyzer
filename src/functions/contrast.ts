523279ca222302d439ccf1aa75ee4d7f25c1c53e
clauds-macedo <yanalogic@gmail.com>
2023-08-09 18:23:27 -0300

    style: simplify const declaration

--- src/functions/contrast.ts
+++ src/functions/contrast.ts
@@ -1,14 +1,11 @@
-import { getLuminance } from "./getLuminance";
-import { hexToRgb } from "./hexToRGB";
+import { getLuminance } from './getLuminance';
+import { hexToRgb } from './hexToRGB';
 
 export const contrast = (color1: string, color2: string): number => {
-    const [r1, g1, b1] = hexToRgb(color1);
-    const [r2, g2, b2] = hexToRgb(color2);
-  
-    const lum1 = getLuminance(r1, g1, b1);
-    const lum2 = getLuminance(r2, g2, b2);
-  
-    return lum1 > lum2
-      ? (lum1 + 0.05) / (lum2 + 0.05)
-      : (lum2 + 0.05) / (lum1 + 0.05);
-  }
+  const [[r1, g1, b1], [r2, g2, b2]] = [hexToRgb(color1), hexToRgb(color2)];
+  const [lum1, lum2] = [getLuminance(r1, g1, b1), getLuminance(r2, g2, b2)];
+
+  return lum1 > lum2
+    ? (lum1 + 0.05) / (lum2 + 0.05)
+    : (lum2 + 0.05) / (lum1 + 0.05);
+};
